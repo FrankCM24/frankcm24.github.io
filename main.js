d22aedfbee776f4bc05f4ce601330e7d4a9090a7
FrankCM24 <134014370+FrankCM24@users.noreply.github.com>
2024-02-21 13:16:28 -0600

    Update and rename main js to main.js
    Added functionality to the menu

---
+++ main.js
@@ -0,0 +1,31 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getelementById('nav-close');
+
+/* Menu Show */
+if(navToggle){
+  navToggle.addEventListener('click', () =>{
+    navMenu.classList.add('show-menu')
+  })
+}
+
+/* Hidden Menu */
+if(navClose){
+  navClose.addEventlistener('click', () =>{
+    navenu.classList.remove('show-menu')
+  })
+}
+/*=============== REMOVE MENU MOBILE ===============*/
+
+
+/*=============== ADD BLUR HEADER ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
