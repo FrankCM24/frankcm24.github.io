0b597788a28ae91fa62094fde785b634b53d354d
FrankCM24 <134014370+FrankCM24@users.noreply.github.com>
2024-02-21 13:42:40 -0600

    Remove Mobile Menu
    Added code to the mobile menu to make it dissapear when you click a link

--- main.js
+++ main.js
@@ -13,11 +13,19 @@ if(navToggle){
 /* Hidden Menu */
 if(navClose){
   navClose.addEventlistener('click', () =>{
-    navenu.classList.remove('show-menu')
+    navMenu.classList.remove('show-menu')
   })
 }
-/*=============== REMOVE MENU MOBILE ===============*/
 
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+      const navMenu = document.getElementById('nav-menu')
+      //When clicked on each nav__link, the class show-menu is removed
+      navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== ADD BLUR HEADER ===============*/
 
